738e3c6ce10348e1dd822a6463f15bac961ca802
Abd2002 <68495742+dualsyscornd@users.noreply.github.com>
2022-03-28 16:35:41 +0530

    Updated

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+const port = 4000;
+
+
+app.get('/', (req, res) => {
+    res.send('The Campus connect is running')
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port :  ${port}`);
+});
