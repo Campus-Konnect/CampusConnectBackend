74ac5f0f1ddf4ba736b56ddb838c2287bbff7aee
Abd2002 <68495742+dualsyscornd@users.noreply.github.com>
2022-06-12 18:34:06 +0530

    Udpated

--- routes/TeacherRoutes.js
+++ routes/TeacherRoutes.js
@@ -2,9 +2,15 @@ const route = require('express').Router();
 
 const AuthMiddleware = require('../middleware/AuthMiddleware')
 // const AuthController = require('../controllers/Teachers/Auth/AuthController');
-const GeneralAPI = require('../controllers/Teachers/GeneralAPI/SchoolDiary.js');
+const GeneralAPI = require('../controllers/Teachers/GeneralAPI/SchoolDiary');
+
+// http://139.59.1.105/
 
 
 
 route.post('/PostRemark', AuthMiddleware.Auth, GeneralAPI.PostRemarks);
 route.get('/GetRemarks', AuthMiddleware.Auth, GeneralAPI.GetRemarks);
+
+
+
+module.exports = route
