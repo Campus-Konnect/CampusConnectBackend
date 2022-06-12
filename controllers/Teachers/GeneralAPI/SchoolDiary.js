8b72e9e69dc6eb0ff582b5b95c366486f0baec40
dualsysco <dualsysco@gmail.com>
2022-06-12 19:30:15 +0530

    Update SchoolDiary.js

--- controllers/Teachers/GeneralAPI/SchoolDiary.js
+++ controllers/Teachers/GeneralAPI/SchoolDiary.js
@@ -25,7 +25,7 @@ exports.PostRemarks = (req, res) => {
 // --- === Post Remark Function === --- \\
 exports.GetRemarks = (req, res) => {
     var start = req.query.page * req.query.limit;
-    db.query('SELECT * FROM general__remarks WHERE school_id=? LIMIT ?, ? ORDER BY remark_id DESC; SELECT CEIL(COUNT(*) / ?) AS TotalPages WHERE school_id=?', [req.query.school_id, start, req.query.limit, start, req.query.limit, req.query.school_id], function (err, data) {
+    db.query('SELECT * FROM general__remarks WHERE school_id=? AND student_id=? LIMIT ?, ? ORDER BY remark_id DESC; SELECT CEIL(COUNT(*) / ?) AS TotalPages WHERE school_id=? AND student_id=?', [req.query.school_id, start, req.query.limit, req.query.student_id, start, req.query.limit, req.query.school_id, req.query.student_id], function (err, data) {
         if (err) {
             console.log(err);
             return res.json({ success: false, message: 'Oops somethinge went went wronge.'+err });
