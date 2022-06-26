de3a36e21a6fb1e75b0f6b95978488910a433842
dualsyscornd <68495742+dualsyscornd@users.noreply.github.com>
2022-06-26 15:23:10 +0530

    Update Config.js

--- config/Config.js
+++ config/Config.js
@@ -1,12 +1,12 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-var con = mysql.createConnection({
+var con = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-})
+});
 
 // con.connect();
 
