39cca046a9a02afd24e118c391f806449573aa77
dualsyscornd <68495742+dualsyscornd@users.noreply.github.com>
2022-07-22 12:22:09 +0530

    Updated

--- config/Config.js
+++ config/Config.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-var con = mysql.createPool({
+var con = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
