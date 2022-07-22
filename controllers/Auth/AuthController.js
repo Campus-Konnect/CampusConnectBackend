540600db2460c5824d71c010d86a5a05794b15b8
dualsyscornd <68495742+dualsyscornd@users.noreply.github.com>
2022-07-22 12:17:48 +0530

    Updated

--- controllers/Auth/AuthController.js
+++ controllers/Auth/AuthController.js
@@ -7,19 +7,23 @@ const Group = require('../../config/Global')
 
 exports.Login = (req, res) => {
     db.query('SELECT * FROM schools WHERE (school_email=? AND school_password=?)', [req.query.login_id, req.query.user_password], function (error, data) {
-        if (error) throw res.json({ success: false, message: "Invalid credentials please retry.." });
-        // --- Success Function --- \\
-        if (data.length > 0) {
-            // --- Auth Token --- \\
-            var token = jwt.sign({ id: data[0].login_id }, process.env.TOKEN_SECRET, {
-                expiresIn: process.env.TOKEN_EXPIRES_IN,
-            });
-            data[0].token = token
-            // --- Auth Token --- \\
-            res.json({ success: true, messsage: "Login successful redirecting..", data: data })
+        if (error) {
+            console.log(error);
+            return res.json({ success: false, message: "Invalid credentials please retry.." });
         } else {
-            res.json({ success: false, message: "Invalid credentials please retry.." })
+            // --- Success Function --- \\
+            if (data.length > 0) {
+                // --- Auth Token --- \\
+                var token = jwt.sign({ id: data[0].login_id }, process.env.TOKEN_SECRET, {
+                    expiresIn: process.env.TOKEN_EXPIRES_IN,
+                });
+                data[0].token = token
+                // --- Auth Token --- \\
+                res.json({ success: true, messsage: "Login successful redirecting..", data: data })
+            } else {
+                res.json({ success: false, message: "Invalid credentials please retry.." })
+            }
+            // --- Success Function --- \\
         }
-        // --- Success Function --- \\
     });
 }
